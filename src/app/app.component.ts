9e7ba336fcddc7eac72c22eaa0e912b76cf7882f
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-28 20:09:58 +0300

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,          // MUST be standalone
-  imports: [ProductsComponent],  // import your ProductsComponent
+  standalone: true,
+  imports: [ProductsComponent],
   template: '<app-products></app-products>',
 })
 export class AppComponent {}
