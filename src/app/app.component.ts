315a82706594e0592d032704138f051f2c67e481
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-28 20:02:18 +0300

    upeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
+import { ProductsComponent } from './products/products.component';
+
 @Component({
   selector: 'app-root',
-  template: '<app-products></app-products>'
+  standalone: true,
+  imports: [ProductsComponent],  // import your component
+  template: '<app-products></app-products>',
 })
 export class AppComponent {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
+// src/app/products/products.component.ts
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products',
+  standalone: true,
+  imports: [FormsModule, NgFor],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: { name: string; price: number }[] = [
+    { name: 'Apple', price: 100 },
+    { name: 'Banana', price: 50 },
+  ];
 
-  products: any[] = [];
   newProductName = '';
-  newProductPrice: number = 0;
-
-  constructor(private productService: ProductService) {}
-
-  ngOnInit() {
-    this.products = this.productService.getProducts();
-  }
+  newProductPrice: number | null = null;
 
   addProduct() {
-    this.products.push({
-      name: this.newProductName,
-      price: this.newProductPrice
-    });
-    this.newProductName = '';
-    this.newProductPrice = 0;
+    if (this.newProductName && this.newProductPrice !== null) {
+      this.products.push({ name: this.newProductName, price: this.newProductPrice });
+      this.newProductName = '';
+      this.newProductPrice = null;
+    }
   }
 
   deleteProduct(index: number) {
