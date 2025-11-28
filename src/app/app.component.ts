bac2edd05f0a2c8a20ecdb85feb26f14c6d15850
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-28 20:04:46 +0300

    upeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductsComponent],  // import your component
+  imports: [ProductsComponent], // import your ProductsComponent
   template: '<app-products></app-products>',
 })
 export class AppComponent {}
